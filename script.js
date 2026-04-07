46b94143bc886003c5feb2bb59a21555fdf1c4db
amonbini <amon.bini@escola.pr.gov.br>
2026-04-07 08:38:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+/ Menu móvel
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
 
+menuToggle.addEventListener('click', () => {
+    nav
